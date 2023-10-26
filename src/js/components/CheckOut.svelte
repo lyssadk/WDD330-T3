d1f1a78de3a3201dd9b9a8b5b3123eff8749eb8c
Alyssa Kucharyski <adkucharyski@gmail.com>
2023-10-26 11:10:02 -0600

    hi

--- src/js/components/CheckOut.svelte
+++ src/js/components/CheckOut.svelte
@@ -1,14 +1,23 @@
 <script>
+  import {getLocalStorage} from "../utils.mjs";
     let tax = 0;
     let shipping = 0;
-    let total = 0;
+    let subtotal = 0;
+    let list = [];
 
-    function calculateItemSummary(){
+    export let key = "so-cart";
 
+    const init = function(){
+      list = getLocalStorage(key);
+      calculateItemSummary();
     }
-    function init(){
-      
+
+    const calculateItemSummary = function(){
+      const amounts = list.map((item)=> item.FinalPrice);
+      subtotal = amounts.reduce((sum, item) => sum + item);
     }
+  
+ init();   
 </script>
 
 <form id="shipping">
@@ -19,13 +28,13 @@
       <label for="lname">Last Name:</label><br>
       <input type="text" name="lname" id="lname"><br>
       <label for="street">Street:</label><br>
-      <input type="text" id="street"><br>
+      <input type="text" name="street" id="street"><br>
       <label for="city">City:</label><br>
-      <input type="text" id="city"><br>
+      <input type="text" name="city" id="city"><br>
       <label for="state">State:</label><br>
-      <input type="text" id="state"><br>
+      <input type="text" name="state" id="state"><br>
       <label for="zip">Zip Code:</label><br>
-      <input type="text" id="zip"><br>
+      <input type="text" name="zip" id="zip"><br>
     </fieldset>
   </form>
 
@@ -34,11 +43,11 @@
     <fieldset>
       <legend>Payment</legend>
       <label for="card_number">Card Number:</label><br>
-      <input type="text" id="card_number"  maxlength="16"><br>
+      <input type="text" name="cardNumber" id="card_number"  maxlength="16"><br>
       <label for="expiration">Expiration:</label><br>
-      <input type="text"  id="expiration"><br>
+      <input type="text" name="Expiration" id="expiration"><br>
       <label for="security">Security Code:</label><br>
-      <input type="text" id="security" maxlength="3"><br>
+      <input type="text" name = "securityCode" id="security" maxlength="3"><br>
     </fieldset>
   </form>
 
@@ -46,7 +55,7 @@
     <legend>Order Summary</legend>
     <div>
       <p>Item Subtotal: </p>
-      <p id="subtotal"></p>
+      <p id="subtotal">${subtotal}</p>
     </div>
     <div>
       <p>Shipping Estimate: </p>
@@ -58,7 +67,7 @@
     </div>
     <div>
       <p>Order Total: </p>
-      <p>${total}</p>
+      <p></p>
     </div>
   </fieldset>
   <button type="submit">Checkout</button>
